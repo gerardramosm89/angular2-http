3a1b2006468848cda3e73f25cf3e5f7199e28514
Cudablogv2 <gerard.ramosm89@gmail.com>
2016-12-25 23:05:12 -0800

    first HTTP call done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { User } from './shared/models/user';
 @Component({
   selector: 'my-app',
   styles: [`
@@ -8,10 +10,26 @@ import { Component } from '@angular/core';
   template: `
     <div class="jumbotron text-center">
       <h1>Gerry's HTTP Practice</h1>
-      <p>{{ message }}</p>
     </div>
-  `
+		<div *ngIf="users">
+			<div *ngFor="let user of users">
+				<h2>{{ user.first_name }}</h2>
+			</div>
+		</div>
+	`
 })
-export class AppComponent {
-  message = 'This is the sample message.';
+export class AppComponent implements OnInit{
+	users: User[];
+	constructor(private http: Http){
+	
+	}
+
+	ngOnInit(){
+		//grab users
+		this.http.get('https://reqres.in/api/users')
+		.subscribe(data => {
+			console.log(data.json());
+			this.users = data.json().data;
+		});
+	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ 
+		BrowserModule,
+		HttpModule,
+		FormsModule
+	],
+  declarations: [ 
+		AppComponent
+ 	],
+	providers: [
+	
+	],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
