e9e0c1e5fe258b37aa3eea51d572a6cbe8e282e8
root <gerard.ramosm89@gmail.com>
2017-01-18 17:01:37 -0800

    changes on user service

--- app/shared/services/user.service.ts
+++ app/shared/services/user.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { User } from '../models/user';
 
 @Injectable()
 
 export class UserService {
-
+	usersUrl: string = 'https://reqres.in/api/users';
 	constructor (private http: Http){
 	}
 	//grab all users
-	getUsers(): Observable<any> {
-	 return this.http.get('https://reqres.in/api/users')
+	getUsers(): Observable<User[]> {
+	 return this.http.get(`${this.usersUrl}`)
 		  .map(res => res.json().data)
 			.catch(err => {
 				return Observable.throw(err.json().data || 'Server error.');
